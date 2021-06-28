cf94663a98433c4592c8e068f851885de03f7bfb
KEN <84188493+Kriszel@users.noreply.github.com>
2021-06-29 01:00:34 +0800

    created Newsapi controller

---
+++ application/controllers/Newsapi.php
@@ -0,0 +1,25 @@
+<?php
+defined ('BASEPATH') OR exit ('no direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class Newsapi extends RestController
+{
+	function__construct()
+	{
+		parent::__contruct();
+		$this->load->database();
+	}
+	
+	public funstion index_get($id=0)
+	{
+		if(!empty($id))
+		{
+			$data = $this->db->get_where('heroku_9a33627d4fcc7f9.newtable', ['id' => $id])->row_array();
+		}
+		else 
+		{
+			$data = $this->db->get('heroku_9a33627d4fcc7f9.newtable')->result();
+		}$this->response($data, 200);
+	}
+}
